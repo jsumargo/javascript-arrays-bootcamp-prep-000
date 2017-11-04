63b2828f1cddcdcb73f73525eb8039409d024547
Jennifer Sumargo <jennifersumargo@yahoo.com>
2017-11-04 10:16:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-  var newArray = [...array, element];
+  var newArray = [element, ...array];
   return newArray;
 }
 
@@ -11,7 +11,7 @@ function destructivelyAddElementToBeginningOfArray(array, element) {
 }
 
 function addElementToEndOfArray(array, element) {
-  var newArray = [element, ...array];
+  var newArray = [...array, element];
   return newArray;
 }
 
